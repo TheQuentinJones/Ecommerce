9bb9ace932ff197151b30e598fd1f9e469455d9d
NEO <qj82189@gmail.com>
2023-02-06 22:55:20 -0500

    Fixed some issues

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: ProductTag }]
+      include: [{ model: Product }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product }],
     });
 
     if (!tagData) {
@@ -51,8 +51,7 @@ router.put('/:id', async (req, res) => {
     {
       // All the fields you can update and the data attached to the request body.
       tag_name: req.body.tag_name,
-      tag_id: req.body.tag_id,
-      
+            
     },
     {
      where: {
